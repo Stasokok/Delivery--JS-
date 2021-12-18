8bf4ae8abffb573aa9c160dcc93bdd741bab9f14
StanislavAkhmetov <76448132+Stasokok@users.noreply.github.com>
2021-12-18 21:45:17 +0300

    Delete OZONE directory

--- OZONE/src/modules/getData.js
+++
@@ -1,8 +0,0 @@
-const getData = () => {
-    return fetch('https://test-6c7d2-default-rtdb.firebaseio.com/goods.json')
-        .then((response) => {
-             return response.json()
-    })
-}
-
-export default getData
